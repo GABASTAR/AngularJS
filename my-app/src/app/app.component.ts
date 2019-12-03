449902b64614e1f45be44651faae113716e11b59
Adam <lolmagamed1872@yandex.ru>
2019-12-03 14:17:54 +0300

    Child added

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,18 +1,5 @@
 import { Component } from '@angular/core';
 
-class Item{
-  purchase: string;
-  done: boolean;
-  price: number;
-   
-  constructor(purchase: string, price: number) {
-
-      this.purchase = purchase;
-      this.price = price;
-      this.done = false;
-  }
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,18 +8,5 @@ class Item{
 
 export class AppComponent { 
   title = 'my-app';
-  text: string;
-  price: number;
-  items: Item[] = 
-  [
-      { purchase: "Хлеб", done: false, price: 25 },
-      { purchase: "Масло", done: false, price: 60 },
-      { purchase: "Картофель", done: false, price: 22.6 },
-      { purchase: "Сыр", done: false, price: 310 }
-  ];
-  addItem(): void {
-      if(this.text==null || this.text.trim()=="" || this.price==null)
-          return;
-      this.items.push(new Item(this.text, this.price));
-  }
+  name = "app"
 }
