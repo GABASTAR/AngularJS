cb477aee4c5a53d0b4d80a0fec468bd58130e201
Adam <lolmagamed1872@yandex.ru>
2019-12-10 13:38:27 +0300

    Routing

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ChildComponent } from './child/child.component'
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './route/home/home.component';
+import { AboutComponent } from './route/about/about.component';
+import { NotFoundComponent } from './route/not-found/not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     FormsModule,
