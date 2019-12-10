9e16a12c1964a1ac05002cacad9c238a984ee60a
Adam <lolmagamed1872@yandex.ru>
2019-12-10 15:01:48 +0300

    Main button

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { Router, } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 
 export class AppComponent { 
   name:string = "Name"
+
+  constructor(private route:Router){}
+    goHome() {
+      this.route.navigate([""])
+    }
 }
