05aa69e5c848d2ba42d61358f67b09aafbb7d047
Adam <lolmagamed1872@yandex.ru>
2019-12-24 15:51:45 +0300

    Login ver.2

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -8,19 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   login: string
   password: string
+  entered: boolean
 
   constructor() { }
 
   ngOnInit() {
     this.login = "adm"
     this.password = "adm123"
+    this.entered = false
   }
   MakeLogin() {
     console.log(this.login)
     console.log(this.password)
     if (this.login == "adm" && this.password == "adm123") {
+      this.entered = true
       console.log("You have entered succesfully")
     }
+    else {
+      this.entered = false
+      localStorage.setItem("login", this.login)
+      localStorage.setItem("password", this.password)
+      const pass = localStorage.getItem("password")
+      const log = localStorage.getItem("login")
+      if (pass === this.login && log === this.password) {
+        this.entered = true;
+        console.log("Nice!")
+      }
+    }
   }
-
+  
 }
